41279f74f03850d7674a2cdb7fa5ae49d823795c
itz-youssef <yoyoyasso19@gmail.com>
2026-05-09 22:00:13 +0300

    Add page transition loader

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,20 @@
-import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { CommonModule, AsyncPipe } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { LoadingService } from './loading.services';
 import { PreloaderComponent } from './preloader/preloader.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, PreloaderComponent, NavbarComponent],
+  imports: [CommonModule, RouterOutlet, PreloaderComponent, NavbarComponent, AsyncPipe],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  loading = false;
+export class AppComponent {
+  hidden$ = this.loadingService.loading$.pipe(map(v => !v));
 
-  constructor(
-    private loadingService: LoadingService,
-    private cdr: ChangeDetectorRef   // ← ADD THIS
-  ) {}
-
-  ngOnInit() {
-    this.loadingService.loading$.subscribe(state => {
-      this.loading = state;
-      this.cdr.detectChanges();      // ← ADD THIS
-    });
-  }
+  constructor(public loadingService: LoadingService) {}
 }
