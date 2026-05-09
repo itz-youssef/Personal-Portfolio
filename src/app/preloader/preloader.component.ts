712e9afe62a2a6431672ba1b9c28a779c683b930
itz-youssef <yoyoyasso19@gmail.com>
2026-05-09 22:03:50 +0300

    Add page transition loader

--- src/app/preloader/preloader.component.ts
+++ src/app/preloader/preloader.component.ts
@@ -1,12 +1,18 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-preloader',
   standalone: true,
   imports: [CommonModule],
-  templateUrl: './preloader.component.html',
-  styleUrls: ['./preloader.component.css']
+  template: `
+    <div id="preloader" *ngIf="!hidden">
+      <div class="loader-container">
+        <div class="spinner"></div>
+        <p>Loading</p>
+      </div>
+    </div>
+  `
 })
 export class PreloaderComponent {
   @Input() hidden = false;
